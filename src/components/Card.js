1c522b35c0788d03658803c60d25c74ce40972a2
cooleydw494 <cooleydw494@gmail.com>
2023-05-08 17:42:23 -0400

    Improved Card.js with comments and defaultProps

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,12 +3,17 @@ import PropTypes from 'prop-types';
 import { View, StyleSheet } from 'react-native';
 import { colors } from '../styles/globalStyles';
 
+// Card component is a reusable container for displaying content with a consistent style
 const Card = ({ children }) => {
   return <View style={styles.card}>{children}</View>;
 };
 
 Card.propTypes = {
   children: PropTypes.node.isRequired,
+};
+
+Card.defaultProps = {
+  children: null,
 };
 
 const styles = StyleSheet.create({
