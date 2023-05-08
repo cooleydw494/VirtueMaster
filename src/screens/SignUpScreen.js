58e39defea7c3cda426cfa4d0da15a62ecda6455
cooleydw494 <cooleydw494@gmail.com>
2023-05-08 16:03:09 -0400

    Improved SignUpScreen.js by using useCallback hook and adding navigation on successful sign up

--- src/screens/SignUpScreen.js
+++ src/screens/SignUpScreen.js
@@ -1,6 +1,6 @@
-import React, { useState } from 'react';
+import React, { useState, useCallback } from 'react';
 import { View, Text, TextInput, TouchableOpacity, StyleSheet, ActivityIndicator, Alert, Linking } from 'react-native';
-import { globalStyles, colors } from '../styles/globalStyles';
+import PropTypes from 'prop-types';
 import { getAuth } from 'firebase/auth';
 import { NavigationContainer, createStackNavigator, createBottomTabNavigator, Ionicons } from '@react-navigation';
 
@@ -23,26 +23,30 @@ const SignUpScreen = ({ navigation }) => {
     return true;
   };
 
-  const signUp = async () => {
+  const signUp = useCallback(async () => {
+    if (!validateForm()) {
+      return;
+    }
     setLoading(true);
     setErrorMessage('');
     try {
-        const auth = getAuth();
+      const auth = getAuth();
       await auth.createUserWithEmailAndPassword(email, password);
       Alert.alert('Successfully signed up!');
       setLoading(false);
+      navigation.navigate('SignInScreen');
     } catch (error) {
       Alert.alert('Error:', error.message);
       setLoading(false);
     }
-  };
+  }, [email, password, navigation]);
 
   return (
-    <View style={globalStyles.container}>
-      <Text style={globalStyles.title}>Sign Up</Text>
-      {errorMessage ? <Text style={globalStyles.errorText}>{errorMessage}</Text> : null}
+    <View style={styles.container}>
+      <Text style={styles.title}>Sign Up</Text>
+      {errorMessage ? <Text style={styles.errorText}>{errorMessage}</Text> : null}
       <TextInput
-        style={globalStyles.input}
+        style={styles.input}
         placeholder='Email'
         onChangeText={setEmail}
         value={email}
@@ -51,7 +55,7 @@ const SignUpScreen = ({ navigation }) => {
         autoCapitalize='none'
       />
       <TextInput
-        style={globalStyles.input}
+        style={styles.input}
         placeholder='Password'
         onChangeText={setPassword}
         value={password}
@@ -60,7 +64,7 @@ const SignUpScreen = ({ navigation }) => {
         autoCapitalize='none'
       />
       <TextInput
-        style={globalStyles.input}
+        style={styles.input}
         placeholder='Confirm Password'
         onChangeText={setConfirmPassword}
         value={confirmPassword}
@@ -68,22 +72,69 @@ const SignUpScreen = ({ navigation }) => {
         autoCompleteType='password'
         autoCapitalize='none'
       />
-      <TouchableOpacity style={globalStyles.primaryButton} onPress={signUp}>
+      <TouchableOpacity style={styles.primaryButton} onPress={signUp}>
         {loading ? (
-          <ActivityIndicator size='small' color={colors.white} />
+          <ActivityIndicator size='small' color='#FFFFFF' />
         ) : (
-          <Text style={globalStyles.buttonText}>Sign Up</Text>
+          <Text style={styles.buttonText}>Sign Up</Text>
         )}
       </TouchableOpacity>
       <TouchableOpacity
         onPress={() => navigation.navigate('SignInScreen')}
       >
-        <Text style={globalStyles.linkText}>
+        <Text style={styles.linkText}>
           Already have an account? Sign In
         </Text>
       </TouchableOpacity>
     </View>
   );
 };
+
+SignUpScreen.propTypes = {
+  navigation: PropTypes.object.isRequired,
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    paddingHorizontal: 20,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 20,
+    textAlign: 'center',
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: '#CCCCCC',
+    borderRadius: 5,
+    paddingHorizontal: 10,
+    paddingVertical: 5,
+    marginBottom: 10,
+  },
+  primaryButton: {
+    backgroundColor: '#007BFF',
+    borderRadius: 5,
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingVertical: 10,
+    marginBottom: 10,
+  },
+  buttonText: {
+    color: '#FFFFFF',
+    fontSize: 16,
+  },
+  linkText: {
+    color: '#007BFF',
+    textAlign: 'center',
+  },
+  errorText: {
+    color: '#FF0000',
+    marginBottom: 10,
+    textAlign: 'center',
+  },
+});
 
 export default SignUpScreen;
