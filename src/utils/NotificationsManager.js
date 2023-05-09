8d34a429c4344df101af88e07f53ae80ab12ca79
cooleydw494 <cooleydw494@gmail.com>
2023-05-09 17:37:10 -0400

    Added error handling to NotificationsManager.js

--- src/utils/NotificationsManager.js
+++ src/utils/NotificationsManager.js
@@ -14,28 +14,36 @@ export async function createChannel() {
 
 // Schedules a daily reminder notification at the specified time
 export async function scheduleDailyReminder(time) {
-  await createChannel();
+  try {
+    await createChannel();
 
-  const trigger = { type: Notifee.Trigger.Type.TIME_INTERVAL, interval: 86400, repeat: true };
-  await Notifee.createTriggerNotification(
-    {
-      id: 'daily-reminder-notification',
-      title: 'Track your progress',
-      body: "Don't forget to track your progress today.",
-      android: {
-        channelId: CHANNEL_ID,
-        smallIcon: 'ic_stat_name',
-        largeIcon: 'ic_launcher',
+    const trigger = { type: Notifee.Trigger.Type.TIME_INTERVAL, interval: 86400, repeat: true };
+    await Notifee.createTriggerNotification(
+      {
+        id: 'daily-reminder-notification',
+        title: 'Track your progress',
+        body: "Don't forget to track your progress today.",
+        android: {
+          channelId: CHANNEL_ID,
+          smallIcon: 'ic_stat_name',
+          largeIcon: 'ic_launcher',
+        },
+        ios: {
+          categoryId: 'DAILY_REMINDER',
+        },
       },
-      ios: {
-        categoryId: 'DAILY_REMINDER',
-      },
-    },
-    { trigger: trigger }
-  );
+      { trigger: trigger }
+    );
+  } catch (error) {
+    console.error('Error scheduling daily reminder:', error);
+  }
 }
 
 // Cancels the scheduled daily reminder notification
 export async function cancelScheduledNotification() {
-  await Notifee.cancelNotification('daily-reminder-notification');
+  try {
+    await Notifee.cancelNotification('daily-reminder-notification');
+  } catch (error) {
+    console.error('Error canceling scheduled notification:', error);
+  }
 }
