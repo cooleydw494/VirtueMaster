1aacd2e2023a8301a3df37f420ff003cd7887ae5
cooleydw494 <cooleydw494@gmail.com>
2023-05-08 23:38:17 -0400

    Added a style prop to Card component for custom styling in Card.js

--- src/components/Card.js
+++ src/components/Card.js
@@ -4,16 +4,18 @@ import { View, StyleSheet } from 'react-native';
 import { colors } from '../styles/globalStyles';
 
 // Card component is a reusable container for displaying content with a consistent style
-const Card = ({ children }) => {
-  return <View style={styles.card}>{children}</View>;
+const Card = ({ children, style }) => {
+  return <View style={[styles.card, style]}>{children}</View>;
 };
 
 Card.propTypes = {
   children: PropTypes.node.isRequired,
+  style: PropTypes.oneOfType([PropTypes.object, PropTypes.array]),
 };
 
 Card.defaultProps = {
   children: null,
+  style: {},
 };
 
 const styles = StyleSheet.create({
