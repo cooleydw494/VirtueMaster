aa62466f3918d0b629730a8639e5c38d8487c2f5
cooleydw494 <cooleydw494@gmail.com>
2023-05-09 22:52:20 -0400

    Split WelcomeScreen.js component into smaller components for better readability and maintainability

--- src/screens/WelcomeScreen.js
+++ src/screens/WelcomeScreen.js
@@ -5,28 +5,40 @@ import { globalStyles } from '../styles/globalStyles';
 import BackgroundImage from '../../assets/custom-svgs/BackgroundImage.js';
 import Logo from '../../assets/custom-svgs/Logo.js';
 
+const WelcomeScreenHeader = () => (
+  <>
+    <BackgroundImage style={globalStyles.background} />
+    <Logo style={globalStyles.logo} />
+    <Text style={globalStyles.title}>VirtueMaster</Text>
+    <Text style={globalStyles.subtitle}>
+      Unlock your potential through the practice of virtues.
+    </Text>
+  </>
+);
+
+const WelcomeScreenButtons = ({ navigation }) => (
+  <>
+    <TouchableOpacity
+      style={globalStyles.primaryButton}
+      onPress={() => navigation.navigate('SignInScreen')}
+    >
+      <Text style={globalStyles.buttonText}>Sign In</Text>
+    </TouchableOpacity>
+    <TouchableOpacity
+      style={globalStyles.secondaryButton}
+      onPress={() => navigation.navigate('SignUpScreen')}
+    >
+      <Text style={globalStyles.buttonText}>Sign Up</Text>
+    </ TouchableOpacity>
+  </>
+);
+
 const WelcomeScreen = React.memo(({ navigation }) => {
   return (
     <View style={globalStyles.container}>
       <View style={globalStyles.overlay}>
-        <BackgroundImage style={globalStyles.background}/>
-        <Logo style={globalStyles.logo} />
-        <Text style={globalStyles.title}>VirtueMaster</Text>
-        <Text style={globalStyles.subtitle}>
-          Unlock your potential through the practice of virtues.
-        </Text>
-        <TouchableOpacity
-          style={globalStyles.primaryButton}
-          onPress={() => navigation.navigate('SignInScreen')}
-        >
-        <Text style={globalStyles.buttonText}>Sign In</Text>
-        </TouchableOpacity>
-        <TouchableOpacity
-          style={globalStyles.secondaryButton}
-          onPress={() => navigation.navigate('SignUpScreen')}
-        >
-          <Text style={globalStyles.buttonText}>Sign Up</Text>
-        </ TouchableOpacity>
+        <WelcomeScreenHeader />
+        <WelcomeScreenButtons navigation={navigation} />
       </View>
     </View>
   );
