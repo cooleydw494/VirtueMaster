51b80891c618cce73661f6551fa7a5cdd07f8e7f
cooleydw494 <cooleydw494@gmail.com>
2023-05-11 17:37:10 -0400

    Updated Header.js with suggestions for improvement

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,35 +3,38 @@ import PropTypes from 'prop-types';
 import { View, Text, StyleSheet } from 'react-native';
 
 // Header component displays a title in a styled container
-const Header = ({ title }) => {
+const Header = ({ title, textColor, backgroundColor }) => {
   return (
-    <View style={styles.headerContainer}>
-      <Text style={styles.headerText}>{title}</Text>
+    <View style={[styles.headerContainer, { backgroundColor }]}>
+      <Text style={[styles.headerText, { color: textColor }]}>{title}</Text>
     </View>
   );
 };
 
 // propTypes and defaultProps are used for type checking and setting default values for the component's props
 Header.propTypes = {
-  title: PropTypes.string
+  title: PropTypes.string,
+  textColor: PropTypes.string,
+  backgroundColor: PropTypes.string
 };
 
 Header.defaultProps = {
-  title: 'Default Title'
+  title: 'App Header',
+  textColor: '#333',
+  backgroundColor: '#f5f5f5'
 };
 
+// The styles object contains the styling for the Header component
 const styles = StyleSheet.create({
   headerContainer: {
     paddingTop: 40,
     paddingBottom: 10,
-    backgroundColor: '#f5f5f5',
     alignItems: 'center',
     justifyContent: 'center'
   },
   headerText: {
     fontSize: 24,
-    fontWeight: 'bold',
-    color: '#333'
+    fontWeight: 'bold'
   }
 });
 
