12e82c7c078f0122c3c6a12e98ad383de6b0f163
cooleydw494 <cooleydw494@gmail.com>
2023-05-11 03:22:37 -0400

    Improve pointsSystem.js based on code analysis suggestions

--- src/utils/pointsSystem.js
+++ src/utils/pointsSystem.js
@@ -1,17 +1,17 @@
-const activities = {
-  check_daily_virtue: { points: 5 },
-  complete_reflection: { points: 10 },
-  reach_weekly_goal: { points: 50 },
+const activities = new Map([
+  ["check_daily_virtue", { points: 5 }],
+  ["complete_reflection", { points: 10 }],
+  ["reach_weekly_goal", { points: 50 }],
   // other activities here...
-};
+]);
 
 /**
  * Calculates the points earned for a given activity type.
  * @param {string} activity_type - The type of activity to calculate points for.
  * @returns {number} The number of points earned for the activity.
  */
-const points_earned = (activity_type) => {
-  return activities[activity_type]?.points || 0;
+const get_points_earned = (activity_type) => {
+  return activities.get(activity_type)?.points || 0;
 };
 
 /**
@@ -21,9 +21,9 @@ const points_earned = (activity_type) => {
  * @param {string} params.activity_type - The type of activity to update points for.
  * @returns {number} The updated points.
  */
-const update_points = ({ points, activity_type }) => {
-  const earned_points = points_earned(activity_type);
+const update_points_based_on_activity = ({ points, activity_type }) => {
+  const earned_points = get_points_earned(activity_type);
   return points + earned_points;
 };
 
-export { points_earned, update_points };
+export { get_points_earned, update_points_based_on_activity };
