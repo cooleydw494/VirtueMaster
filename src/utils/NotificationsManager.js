2843a9db31ce9571ad4084ee57fa368c22918eb5
cooleydw494 <cooleydw494@gmail.com>
2023-05-12 03:02:20 -0400

    Improved NotificationsManager.js by updating imports, adding error handling, and adding comments

--- src/utils/NotificationsManager.js
+++ src/utils/NotificationsManager.js
@@ -1,65 +1,66 @@
-import Constants from 'expo-constants';
-import * as Notifications from 'expo-notifications';
+import { Constants } from "expo-constants";
+import { Notifications } from "expo-notifications";
+import { Platform, Alert } from "react-native";
 
-const CHANNEL_ID = 'daily-reminder';
-const NOTIFICATION_ID = 'daily-reminder-notification';
+const CHANNEL_ID = "daily-reminder";
+const NOTIFICATION_ID = "daily-reminder-notification";
 
 // Set notification handler
 Notifications.setNotificationHandler({
-    handleNotification: async () => ({
-        shouldShowAlert: true,
-        shouldPlaySound: false,
-        shouldSetBadge: false,
-    }),
+  handleNotification: async () => ({
+    shouldShowAlert: true,
+    shouldPlaySound: false,
+    shouldSetBadge: false,
+  }),
 });
 
 // Register for push notifications
 export async function registerForPushNotificationsAsync() {
-    let token;
-    if (Constants.isDevice) {
-        const { status: existingStatus } = await Notifications.getPermissionsAsync();
-        let finalStatus = existingStatus;
-        if (existingStatus !== 'granted') {
-            const { status } = await Notifications.requestPermissionsAsync();
-            finalStatus = status;
-        }
-        if (finalStatus !== 'granted') {
-            alert('Failed to get push token for push notification!');
-            return;
-        }
-        token = (await Notifications.getExpoPushTokenAsync()).data;
-    } else {
-        alert('Must use physical device for Push Notifications');
+  let token;
+  if (Constants.isDevice) {
+    const { status: existingStatus } = await Notifications.getPermissionsAsync();
+    let finalStatus = existingStatus;
+    if (existingStatus !== "granted") {
+      const { status } = await Notifications.requestPermissionsAsync();
+      finalStatus = status;
     }
-
-    if (Platform.OS === 'android') {
-        Notifications.setNotificationChannelAsync(CHANNEL_ID, {
-            name: 'Daily Reminder',
-            importance: Notifications.AndroidImportance.MAX,
-            vibrationPattern: [0, 250, 250, 250],
-            lightColor: '#FF231F7C',
-        });
+    if (finalStatus !== "granted") {
+      Alert.alert("Failed to get push token for push notification!");
+      return;
     }
+    token = (await Notifications.getExpoPushTokenAsync()).data;
+  } else {
+    Alert.alert("Must use physical device for Push Notifications");
+  }
 
-    return token;
+  if (Platform.OS === "android") {
+    Notifications.setNotificationChannelAsync(CHANNEL_ID, {
+      name: "Daily Reminder",
+      importance: Notifications.AndroidImportance.MAX,
+      vibrationPattern: [0, 250, 250, 250],
+      lightColor: "#FF231F7C",
+    });
+  }
+
+  return token;
 }
 
 // Schedules a daily reminder notification at the specified time
 export async function scheduleDailyReminder(time) {
-    await Notifications.scheduleNotificationAsync({
-        content: {
-            title: 'Track your progress',
-            body: "Don't forget to track your progress today.",
-        },
-        trigger: {
-            hour: time.hour,
-            minute: time.minute,
-            repeats: true,
-        },
-    });
+  await Notifications.scheduleNotificationAsync({
+    content: {
+      title: "Track your progress",
+      body: "Don't forget to track your progress today.",
+    },
+    trigger: {
+      hour: time.hour,
+      minute: time.minute,
+      repeats: true,
+    },
+  });
 }
 
 // Cancels the scheduled daily reminder notification
 export async function cancelScheduledNotification() {
-    await Notifications.cancelScheduledNotificationAsync(NOTIFICATION_ID);
+  await Notifications.cancelScheduledNotificationAsync(NOTIFICATION_ID);
 }
