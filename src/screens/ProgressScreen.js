d795ff4184eaccb991553b30043665d277a01633
cooleydw494 <cooleydw494@gmail.com>
2023-05-11 20:40:39 -0400

    Moved variables to state in ProgressScreen.js

--- src/screens/ProgressScreen.js
+++ src/screens/ProgressScreen.js
@@ -1,11 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { View, Text, FlatList } from 'react-native';
 import ProgressChart from '../components/ProgressChart';
 import { globalStyles, colors } from '../styles/globalStyles';
 
 const ProgressScreen = () => {
-  const virtueProgressData = [
+  const [virtueProgressData, setVirtueProgressData] = useState([
     { virtue: 'Humility', progress: 0.7 },
     { virtue: 'Patience', progress: 0.5 },
     { virtue: 'Charity', progress: 0.6 },
@@ -13,10 +13,10 @@ const ProgressScreen = () => {
     { virtue: 'Temperance', progress: 0.4 },
     { virtue: 'Diligence', progress: 0.3 },
     { virtue: 'Chastity', progress: 0.9 },
-  ];
+  ]);
 
-  const userPoints = 123;
-  const userRewards = 5;
+  const [userPoints, setUserPoints] = useState(123);
+  const [userRewards, setUserRewards] = useState(5);
 
   return (
       <View style={globalStyles.container}>
@@ -33,15 +33,6 @@ const ProgressScreen = () => {
         />
       </View>
   );
-};
-
-ProgressScreen.propTypes = {
-  userPoints: PropTypes.number.isRequired,
-  userRewards: PropTypes.number.isRequired,
-  virtueProgressData: PropTypes.arrayOf(PropTypes.shape({
-    virtue: PropTypes.string.isRequired,
-    progress: PropTypes.number.isRequired,
-  })).isRequired,
 };
 
 export default ProgressScreen;
